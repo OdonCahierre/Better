c796caf70e908bd4e337b90f686f4b400876ca58
OdonCahierre <windward_student@school.windwardschool.org>
2016-09-02 08:49:20 -0700

    common errors and comments

---
+++ src/MuchBetter.java
@@ -0,0 +1,14 @@
+
+public class MuchBetter {
+	public static void main(String[] args){
+
+		System.out.println("   @@@@@@@ \n  @@@@@@@@@@ \n @@@@@@@@@@@@ \n@@@@@@@@@@@@@@@");
+		System.out.println(" ||||[\"\"\"\"]|||| \n ||||[....]||||");
+		System.out.println("OOOOOOOOOOOOOOOO \n OOOOOOOOOOOOOO \n  OOOOOOOOOOOO \n   OOOOOOOOOO");
+/*
+ * This Pokeball graphic can be used in the opening sequence of my Pokemon game, or any time the player needs to catch a wild Pokemon.
+ */
+	
+	}
+
+}
